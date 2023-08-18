d4b782f9c85cf67169316b8ca216710ed6874d60
Lovebin Joseph Robin <98700499+lovebin123@users.noreply.github.com>
2023-08-18 23:48:39 +0530

    changed font size

--- src/Components/Toggle1.js
+++ src/Components/Toggle1.js
@@ -6,7 +6,7 @@ function Toggle1() {
     <div className='toggle'>
         <div className='toggle-wrapper'>
           <div className='toggler'>
-            <b>Display total price</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div className='vr'></div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Include all fees before taxes <div className='toggler1'><Toggle/></div>
+            <p className='p15'>Display total price</p><div className='vr'></div><p className='p17'>Include all fees before taxes</p> <div className='toggler1'><Toggle/></div>
         </div></div>
         <br></br>
     </div>
