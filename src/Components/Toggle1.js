15196c04d1181395788bc4d3f18942d28a224de9
Lovebin Joseph Robin <98700499+lovebin123@users.noreply.github.com>
2023-08-21 19:50:37 +0530

    changed

--- src/Components/Toggle1.js
+++ src/Components/Toggle1.js
@@ -5,8 +5,8 @@ function Toggle1() {
     <div className='container  my-9   w-25 rounded-4 h-20 border border-black '>
         
           <div className='toggler'>
-            <p className='p15'>Display total price</p><div className='vr'></div><p className='p17'>Include all fees before taxes</p> <p className='toggler1'>      <input type="checkbox" id="switch" /><label for="switch">Toggle</label>
-</p>
+            <p className='p15'>Display total price</p><div className='vr'></div><p className='p17'>Include all fees before taxes &nbsp;&nbsp;&nbsp;&nbsp;   <input type="checkbox" id="switch" /><label for="switch">Toggle</label></p>
+
         </div>
         <br></br>
     </div>
