e55b36c28060434451251eaead727d273ff28c66
Lovebin Joseph Robin <98700499+lovebin123@users.noreply.github.com>
2023-08-21 19:23:56 +0530

    changed toggle

--- src/Components/Toggle1.js
+++ src/Components/Toggle1.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Toggle from './Toggle'
 import '../Components/Toggle.css'
 function Toggle1() {
   return (
     <div className='container  my-9   w-25 rounded-4 h-20 border border-black '>
         
           <div className='toggler'>
-            <p className='p15'>Display total price</p><div className='vr'></div><p className='p17'>Include all fees before taxes</p> <p className='toggler1'><Toggle/></p>
+            <p className='p15'>Display total price</p><div className='vr'></div><p className='p17'>Include all fees before taxes</p> <p className='toggler1'>      <input type="checkbox" id="switch" /><label for="switch">Toggle</label>
+</p>
         </div>
         <br></br>
     </div>
